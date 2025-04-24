8b9319b19587199da4581fc92d30e84506d3e7b5
emna <emna.chebby19@gmail.com>
2025-04-24 19:57:32 +0100

    committed

--- src/main/java/tn/pi/artgallery/config/CORSFilter.java
+++ src/main/java/tn/pi/artgallery/config/CORSFilter.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
+
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
     @Override
